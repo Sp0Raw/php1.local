2dce5bfb9c897d57639c13a7b4cac1dc02419ed0
dobrynin <letun9@mail.ru>
2018-07-17 22:57:32 +0500

    Тест изменений
    (cherry picked from commit c4f35fee7159b417f7dca72de6a2d96a2d1e9424)
    
    # Conflicts:
    #	index.php
    #	login.php

--- image.php
+++ image.php
@@ -10,17 +10,20 @@ session_start();
  * Date: 21.06.2018
  * Time: 9:20
  */
-$arr = [1 => 'cal.jpg',
-    2 => 'pap.jpg',
-    3 => 'flo.jpg',
-    4 => 'grl.jpg',
-    5 => 'fsh.jpg'];
+
+$imgDir = __DIR__.'\img';
+$arr = scandir($imgDir);
+//$arr = [1 => 'cal.jpg',
+//    2 => 'pap.jpg',
+//    3 => 'flo.jpg',
+//    4 => 'grl.jpg',
+//    5 => 'fsh.jpg'];
 $numImg = $_GET['id'] ?? null;
 
-$template1 = '<img src="/img/<!--IMG-->" >';
+$template1 = '<a href="/index.php"><img src="/img/<!--IMG-->" ></a>';
 
 if ($_GET['id'] != null) {
-    echo '<img src="/img/' . $arr[$_GET['id']] . '" >';
-    //echo str_replace('<!--IMG-->', $arr[$numImg], $template1);
+    //echo '<img src="/img/' . $arr[$_GET['id']] . '" >';
+    echo str_replace('<!--IMG-->', $arr[$numImg], $template1);
 }
 
--- upload.php
+++ upload.php
@@ -1,9 +1,4 @@
 <?php
-session_start();
-
-?>
-
-<?php
 /**
  * Created by PhpStorm.
  * User: dobrynin
@@ -11,14 +6,18 @@ session_start();
  * Time: 14:44
  */
 
-var_dump($_FILES);
-echo __DIR__.'/picture/test.jpg';
+//var_dump($_FILES);
+//echo __DIR__.'/picture/test.jpg';
 
 if (isset($_FILES['picture'])){
     if(0==$_FILES['picture']['error']){
         move_uploaded_file(
             $_FILES['picture']['tmp_name'],
-            __DIR__.'/pictures/'.$_FILES['picture']['name']
+            __DIR__.'/img/'.$_FILES['picture']['name']
         );
+        $fNameLog  = 'imgUpload.log';
+        $fLog = fopen($fNameLog,'a');
+        fwrite($fLog,$_FILES['picture']['name'].'   user='.$_SESSION['username'].'   '.date('l jS \of F Y h:i:s A')."\r\n" );
+        fclose($fLog);
     }
 }
